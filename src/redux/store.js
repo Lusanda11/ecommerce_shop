3da320db417aae5a5cad08797e2199dd3fbf6b61
lusanda287 <lusanda287@gmail.com>
2026-01-08 15:18:42 +0200

    updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,26 @@
 // ****************** store_JS *********************
 
 
-
 // Importing necessary functions and modules from Redux and React-Redux libraries.
 // ----------------------------------------------------------------------------------------------------------------------------
-    // import { createStore, combineReducers } from "redux";
-    // import { Provider } from "react-redux";
-    // import shippingReducer from "./reducers/shippingReducer";
+    import { createStore } from "redux"; // Redux core function to create the store.
+    import rootReducer from "./reducers/rootReducer"; // Root reducer that combines all reducers in your app.
+    import { persistStore, persistReducer } from "redux-persist"; // Redux Persist utilities.
+    import storage from "redux-persist/lib/storage"; // Default storage (localStorage for web).
 // ----------------------------------------------------------------------------------------------------------------------------
 
-// Combining multiple reducers into a single root reducer.
+// Configuration for Redux Persist.
 // ----------------------------------------------------------------------------------------------------------------------------
-    // const rootReducer = combineReducers(
-    // {
-    //     shipping: shippingReducer,
-    // });
-    //
-    // const store = createStore(rootReducer); // Creating the Redux store with the root reducer.
+    const persistConfig = {
+        key: "root",      // Key for persisted data in storage
+        storage,          // Where to store the persisted state
+    };
 // ----------------------------------------------------------------------------------------------------------------------------
 
-// export default store; // Exporting the store for use in other parts of the application.
-
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./reducers/rootReducer";
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const store = createStore(persistedReducer);
-const persistor = persistStore(store);
-
-export { store, persistor };
+// Set up Redux store with persisted state.
+// ----------------------------------------------------------------------------------------------------------------------------
+    const persistedReducer = persistReducer(persistConfig, rootReducer); // Wrap the root reducer with persistence capabilities
+    const store = createStore(persistedReducer); // Create the Redux store using the persisted reducer
+    const persistor = persistStore(store);// Create the persistor, which controls persistence lifecycle
+    export { store, persistor }; // Export store and persistor for use in index.js / main.jsx
+// ----------------------------------------------------------------------------------------------------------------------------
